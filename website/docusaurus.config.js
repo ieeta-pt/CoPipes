435f96845c01f2ce2992b6a9098658b2e6dfd30a
Raquel Paradinha <raquelparadinha@gmail.com>
2025-07-22 12:21:39 +0100

    Update Docusaurus config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -23,12 +23,12 @@ const config = {
   url: 'https://joaorafaelalmeida.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/CoPipes/website/',
+  baseUrl: '/thesis-dev/website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'joaorafaelalmeida', // Usually your GitHub org/user name.
-  projectName: 'CoPipes', // Usually your repo name.
+  projectName: 'thesis-dev', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
